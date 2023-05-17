1d851b77a36202b321d76db2c7dd9455c5b37f37
Joni Rodriguez <57025021+jonirodriguez@users.noreply.github.com>
2023-05-17 01:39:40 -0700

    Update ReportGenerator.java
    Fix spacing issue in console display...

--- src/week2/FinalAssignment/ReportGenerator.java
+++ src/week2/FinalAssignment/ReportGenerator.java
@@ -17,6 +17,7 @@ public class ReportGenerator {
 				lowEndProduct = true;
 				
 				if ((lowEndProduct == true) && (product.getQuantity() <= 20)) {
+					System.out.println("");
 					System.out.println("LOW-END PRODUCT STOCK ALERT");
 					System.out.println("Product ID " + product.getId() + "(" + product.getName() + ") has a quantity of " + product.getQuantity() + "!");
 					System.out.println("As a reminder, since this is a product costing less than $100, any quantity below [20] is considered LOW STOCK.");
@@ -27,6 +28,7 @@ public class ReportGenerator {
 				highEndProduct = true;
 				
 				if ((highEndProduct == true) && (product.getQuantity() <= 10)) {
+					System.out.println("");
 					System.out.println("HIGH-END PRODUCT STOCK ALERT");
 					System.out.println("Product ID " + product.getId() + "(" + product.getName() + ") has a quantity of " + product.getQuantity() + "!");
 					System.out.println("As a reminder, since this is a product costing more than $101, any quantity below [10] is considered LOW STOCK.");
