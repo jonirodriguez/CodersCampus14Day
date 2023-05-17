5c6a9f8bfd5c3bb4883753a0fc48ed661386e4d6
Joni Rodriguez <57025021+jonirodriguez@users.noreply.github.com>
2023-05-17 01:40:31 -0700

    Update ReportGenerator.java

--- src/week2/FinalAssignment/ReportGenerator.java
+++ src/week2/FinalAssignment/ReportGenerator.java
@@ -19,7 +19,7 @@ public class ReportGenerator {
 				if ((lowEndProduct == true) && (product.getQuantity() <= 20)) {
 					System.out.println("");
 					System.out.println("LOW-END PRODUCT STOCK ALERT");
-					System.out.println("Product ID " + product.getId() + "(" + product.getName() + ") has a quantity of " + product.getQuantity() + "!");
+					System.out.println("Product ID " + product.getId() + " (" + product.getName() + ") has a quantity of " + product.getQuantity() + "!");
 					System.out.println("As a reminder, since this is a product costing less than $100, any quantity below [20] is considered LOW STOCK.");
 				}
 			}
@@ -30,7 +30,7 @@ public class ReportGenerator {
 				if ((highEndProduct == true) && (product.getQuantity() <= 10)) {
 					System.out.println("");
 					System.out.println("HIGH-END PRODUCT STOCK ALERT");
-					System.out.println("Product ID " + product.getId() + "(" + product.getName() + ") has a quantity of " + product.getQuantity() + "!");
+					System.out.println("Product ID " + product.getId() + " (" + product.getName() + ") has a quantity of " + product.getQuantity() + "!");
 					System.out.println("As a reminder, since this is a product costing more than $101, any quantity below [10] is considered LOW STOCK.");
 				}
 			}
